8664ed1b505baf5e582549c6f6b0bea62567cf43
NicoZapa <zapanicolas@gmail.com>
2021-10-10 11:32:57 -0300

    mi primer commit

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+abstract public class Animal {
+    protected String nombre;
+    protected float peso;
+    protected String especie;
+
+    public Animal(String nombre, float peso, String especie){
+        this.nombre = nombre;
+        this.peso = peso;
+        this.especie = especie;
+    }
+}
---
+++ src/Gato.java
@@ -0,0 +1,8 @@
+public class Gato extends Animal{
+
+    protected String especie;
+
+    public Gato(String nombre, float peso){
+        super(nombre, peso, "Gato");
+    }
+}
